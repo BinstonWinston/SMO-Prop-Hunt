3c1943da000ac0bc7196b5496c725e77e2df6db8
Binston <icyiceberg@no-reply.github.com>
2023-10-28 10:33:19 -0700

    romfs

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ BASE_RYU_PATH="/mnt/c/Users/Binston/AppData/Roaming/Ryujinx/sdcard"
 make -j && \
 cp starlight_patch_100/atmosphere/exefs_patches/StarlightBase/3CA12DFAAF9C82DA064D1698DF79CDA1.ips "$BASE_RYU_PATH/atmosphere/exefs_patches/StarlightBase"
 cp starlight_patch_100/atmosphere/contents/0100000000010000/exefs/subsdk1 "$BASE_RYU_PATH/atmosphere/contents/0100000000010000/exefs"
+cp -r starlight_patch_100/atmosphere/contents/0100000000010000/romfs "$BASE_RYU_PATH/atmosphere/contents/0100000000010000"
